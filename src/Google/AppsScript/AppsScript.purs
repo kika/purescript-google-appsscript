b596a750d57366afb72819d7d4d73a06c81f8782
Kirill Pertsev <kika@rackmaze.com>
2016-05-24 18:42:49 -0700

    Introduced 'programmable semicolon'

--- src/Google/AppsScript/AppsScript.purs
+++ src/Google/AppsScript/AppsScript.purs
@@ -2,11 +2,15 @@ module Google.AppsScript.AppsScript
 (
     GAS
   , GASEff
+  , (:)
 ) 
 where
 
+import Prelude (bind)
 import Control.Monad.Eff
 
 -- | Effect type for Google AppsScript
 foreign import data GAS :: !
 type GASEff a = forall e. Eff (gas::GAS|e) a
+
+infixl 1 bind as :
