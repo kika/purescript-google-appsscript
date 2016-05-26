617c884067e1a850811ca2c14ae609112aefeca6
Kirill Pertsev <kika@rackmaze.com>
2016-05-26 14:25:37 -0700

    Some Range functions

--- src/Google/AppsScript/SpreadsheetApp.js
+++ src/Google/AppsScript/SpreadsheetApp.js
@@ -12,3 +12,51 @@ exports.getUi = function (app) {
     }
 }
 
+exports.getActiveRange = function (app) {
+    return function () {
+        return app.getActiveRange();
+    }
+} // SpreadsheetApp -> GASEff Range
+
+exports.getColumn = function (range) {
+    return function () {
+        return range.getColumn();
+    }
+} // Range -> GASEff Int
+
+exports.getCellImpl = function (range, row, col) {
+    return function () {
+        return range.getCell( row, col );
+    }
+} // Range -> Row -> Column -> GASEff Range
+
+exports.getLastColumn = function (range) {
+    return function () {
+        return range.getLastColumn();
+    }
+} // Range -> GASEff Column
+
+exports.getLastRow = function (range) {
+    return function () {
+        return range.getLastRow();
+    }
+} // Range -> GASEff Row
+
+exports.getNumRows = function (range) { 
+    return function () {
+        return range.getNumRows();
+    }
+} // Range -> GASEff Int
+
+exports.getNumColumns = function (range) {
+    return function () {
+        return range.getNumColumns();
+    }
+} // Range -> GASEff Int
+
+exports.getA1Notation = function (range) {
+    return function () {
+        return range.getA1Notation();
+    }
+}
+
--- src/Google/AppsScript/SpreadsheetApp.purs
+++ src/Google/AppsScript/SpreadsheetApp.purs
@@ -1,17 +1,49 @@
 module Google.AppsScript.SpreadsheetApp 
 (
     SpreadsheetApp
+  , Row
+  , Column
+  , Sheet
+  , Range
   , app
   , getUi
+  , getColumn
+  , getCell
+  , getLastColumn
+  , getLastRow
+  , getNumRows
+  , getNumColumns  
+  , getActiveRange
+  , getA1Notation
 ) where
 
 import Prelude
+import Data.Function
+
 import Google.AppsScript.AppsScript (GAS, GASEff)
 import Google.AppsScript.Ui (Ui)
 import Google.AppsScript.Menu (Menu)
 
 foreign import data SpreadsheetApp :: *
+foreign import data Sheet :: *
+foreign import data Range :: *
+
+type Row    = Int
+type Column = Int
 
 foreign import app::GASEff SpreadsheetApp
 foreign import getUi::SpreadsheetApp -> GASEff Ui
 
+-- Range functions
+foreign import getActiveRange::SpreadsheetApp -> GASEff Range
+foreign import getColumn::Range -> GASEff Int
+foreign import getCellImpl::Fn3 Range Row Column (GASEff Range)
+foreign import getLastColumn::Range -> GASEff Column
+foreign import getLastRow::Range -> GASEff Row
+foreign import getNumRows::Range -> GASEff Int
+foreign import getNumColumns::Range -> GASEff Int
+foreign import getA1Notation::Range -> GASEff String
+foreign import getValue::Range -> GASEff Object
+
+getCell::Row -> Column -> Range -> GASEff Range
+getCell row col range = runFn3 getCellImpl range row col 
