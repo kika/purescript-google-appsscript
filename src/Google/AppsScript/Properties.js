cb8eec2f072b105a8a0e372763d1dbff4196c4fe
Kirill Pertsev <kika@rackmaze.com>
2016-06-06 02:52:54 -0700

    WIP: PropertyService

--- src/Google/AppsScript/Properties.js
+++ src/Google/AppsScript/Properties.js
@@ -2,53 +2,47 @@
 "use strict";
 
 exports.getDocProps = function () {
-    function () {
-        return PropertiesService.getDocumentProperties();
-    }
+    return PropertiesService.getDocumentProperties();
 } // ::GASEff Properties
 exports.getScriptProps = function () {
-    function () {
-        return PropertiesService.getScriptProperties();
-    }
+    return PropertiesService.getScriptProperties();
 } // ::GASEff Properties
 exports.getUserProps = function () {
-    function () {
-        return PropertiesService.getUserProperties();
-    }
+    return PropertiesService.getUserProperties();
 } // ::GASEff Properties
 
 exports.delAll = function (p) {
-    function () {
+    return function () {
         return p.deleteAllProperties();
     }
 } //::Properties -> GASEff Properties
-exports.del = function (key, p) {
-    function () {
+exports.delImpl = function (key, p) {
+    return function () {
         return p.deleteProperty(key);
     }
 } //::String -> Properties -> GASEff Properties
 exports.keys = function (p) {
-    function () {
+    return function () {
         return p.getKeys();
     }
 } //::Properties -> GASEff (Array String)
 exports.getAll = function (p) {
-    function () {
+    return function () {
         return p.getProperties();
     }
 } //::Properties -> GASEff Props 
-exports.get = function (key, p) {
-    function () {
+exports.getImpl = function (key, p) {
+    return function () {
         return p.getProperty(key);
     }
 } //::String -> Properties -> GASEff String
-exports.setAll = function (props, p) {
-    function () {
+exports.setAllImpl = function (props, p) {
+    return function () {
         return p.setProperties(props);
     }
 } //::Props -> Properties -> GASEff Properties 
-exports.set = function (key, value, p) {
-    function () {
+exports.setImpl = function (key, value, p) {
+    return function () {
         return p.setProperty(key, value);
     }
 } //::String -> String -> Properties -> GASEff Properties
--- src/Google/AppsScript/Properties.purs
+++ src/Google/AppsScript/Properties.purs
@@ -14,6 +14,7 @@ module Google.AppsScript.Properties
   , set
 ) where
 
+import Data.Function 
 import Data.StrMap (StrMap)
 import Google.AppsScript.AppsScript (GASEff)
 
@@ -25,9 +26,18 @@ foreign import getScriptProps::GASEff Properties
 foreign import getUserProps::GASEff Properties
 
 foreign import delAll::Properties -> GASEff Properties
-foreign import del::String -> Properties -> GASEff Properties
+foreign import delImpl:: Fn2 String Properties (GASEff Properties)
 foreign import keys::Properties -> GASEff (Array String)
 foreign import getAll::Properties -> GASEff Props 
-foreign import get::String -> Properties -> GASEff String
-foreign import setAll::Props -> Properties -> GASEff Properties 
-foreign import set::String -> String -> Properties -> GASEff Properties
+foreign import getImpl:: Fn2 String Properties (GASEff String)
+foreign import setAllImpl:: Fn2 Props Properties (GASEff Properties)
+foreign import setImpl:: Fn3 String String Properties (GASEff Properties)
+
+set::String -> String -> Properties -> GASEff Properties
+set k v p = runFn3 setImpl k v p
+setAll::Props -> Properties -> GASEff Properties
+setAll pp p = runFn2 setAllImpl pp p
+get::String -> Properties -> GASEff String
+get k p = runFn2 getImpl k p
+del::String -> Properties -> GASEff Properties
+del k p = runFn2 delImpl k p 
